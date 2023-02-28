2145e94ffc68258ae748a20b08694f799f30fd6f
Alvaro <alvaroacosta132@gmail.com>
2023-02-28 15:20:15 -0400

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo'; // aqui se eleva arriba del codigo de esta forma = var nameOfDog; //undefined
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito' // el mejor perrito es undefined
